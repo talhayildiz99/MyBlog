1ba06bc6bfea44c65b3a8fd9e0009e2783354c9d
Talha <99talhayildiz@gmail.com>
2024-03-15 14:58:54 +0300

    Crreated the Context

---
+++ MyBlog.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.DataAccessLayer.Context
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-DSQNOEI\\SQLEXPRESS03; initial catalog = DbMyBlog; integrated security = true");
+        }
+    }
+}
