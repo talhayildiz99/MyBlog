b963392117a6e49615690e77a7fdceb4b3bc9ecf
Talha <99talhayildiz@gmail.com>
2024-04-04 21:43:56 +0300

    Social Media Component Partial

---
+++ MyBlog-PresentationLayer/ViewComponents/DefaultViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog_PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _AboutComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
